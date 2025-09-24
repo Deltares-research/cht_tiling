1537fcb574e75d36e549097863e24f23c5d9d872
Luuk Blom <luuk.blom@deltares.nl>
2025-09-24 17:28:19 +0200

    bump v1.0.3

--- cht_tiling/__init__.py
+++ cht_tiling/__init__.py
@@ -5,7 +5,7 @@ Created on Sun Apr 25 10:58:08 2021
 @author: ormondt
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from cht_tiling.flood_map import FloodMap
 from cht_tiling.tiled_web_map import TiledWebMap
